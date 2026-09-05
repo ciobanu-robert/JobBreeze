ab4680097394b432c1ec094b4800d40c8d7d9457
ciobanu-robert <ciobanurobert100@gmail.com>
2026-09-05 19:53:24 +0300

    Created home page + dark&light themes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./pages/home/home";
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
